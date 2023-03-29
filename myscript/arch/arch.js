e2a6d27fd21a7d851a6944c34d41190fdbdcc5d1
moganda12 <115957608+moganda12@users.noreply.github.com>
2023-03-29 16:37:29 -0700

    html fix

--- myscript/arch/arch.js
+++ myscript/arch/arch.js
@@ -1,7 +1,10 @@
 const str = s => targetString => {
     if(targetString.startsWith(s)) {
         //.........
+        return(s)
+    } else {
+        throw new Error(`Tried to match ${s.slice(0,10)} but got ${targetString.slice(0, len(s))}`)
     }
 };
 
-const test = str('hello')
+const test = str('hello');
